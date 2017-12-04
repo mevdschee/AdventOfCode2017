8aaddebe1f586eaf407e6a0d1159987e29f6c386
Maurits van der Schee <maurits@vdschee.nl>
2017-12-04 06:19:01 +0100

    Day 4

--- day4/src/Question1.java
+++ day4/src/Question1.java
@@ -8,19 +8,19 @@ public class Question1 {
 	public static void main(String[] args) throws IOException {
 		String input = new String(Files.readAllBytes(Paths.get("input"))).trim();
 		String[] lines = input.split("\\n");
-		int duplicates = 0;
+		int invalid = 0;
 		for (int i = 0; i < lines.length; i++) {
 			String[] words = lines[i].trim().split("\\s+");
 			HashMap<String, Boolean> unique = new HashMap<>();
 			for (String word : words) {
 				if (unique.containsKey(word)) {
-					duplicates++;
+					invalid++;
 					break;
 				}
 				unique.put(word, true);
 			}
 		}
-		System.out.println(lines.length - duplicates);
+		System.out.println(lines.length - invalid);
 	}
 
 }
