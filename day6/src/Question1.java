977d39b69844fe7128b1e35fddc107a72a8ba634
Maurits van der Schee <maurits@vdschee.nl>
2017-12-06 16:33:26 +0100

    Day 6

--- day6/src/Question1.java
+++ day6/src/Question1.java
@@ -10,28 +10,22 @@ public class Question1 {
 		String input = new String(Files.readAllBytes(Paths.get("input"))).trim();
 		String[] numbers = input.split("\\s+");
 		int[] banks = new int[numbers.length];
-		for (int i = 0; i < numbers.length; i++) {
+		for (int i = 0; i < banks.length; i++) {
 			banks[i] = Integer.valueOf(numbers[i]);
 		}
 		HashMap<String, Boolean> states = new HashMap<>();
 		int steps = 0;
 		while (true) {
 			int highest = 0;
-			for (int i = 1; i < numbers.length; i++) {
+			for (int i = 1; i < banks.length; i++) {
 				if (banks[i] > banks[highest]) {
 					highest = i;
 				}
 			}
-			int add = (int) Math.ceil((float) banks[highest] / numbers.length);
-			for (int i = 0; i < numbers.length; i++) {
-				if (banks[highest] >= add) {
-					banks[highest] -= add;
-					banks[(highest + 1 + i) % numbers.length] += add;
-				} else {
-					int left = banks[highest];
-					banks[highest] -= left;
-					banks[(highest + 1 + i) % numbers.length] += left;
-				}
+			int add = banks[highest];
+			banks[highest] = 0;
+			for (int i = 0; i < add; i++) {
+				banks[(highest + 1 + i) % banks.length]++;
 			}
 			steps++;
 			String key = Arrays.toString(banks);
