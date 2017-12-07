ed974515a5dc442e1565105d9fe6c8b243357aea
Maurits van der Schee <maurits@vdschee.nl>
2017-12-07 11:50:09 +0100

    Day 7

--- day7/src/Question1.java
+++ day7/src/Question1.java
@@ -1,7 +1,6 @@
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
-import java.util.ArrayList;
 import java.util.HashMap;
 
 public class Question1 {
@@ -10,20 +9,25 @@ public class Question1 {
 		String input = new String(Files.readAllBytes(Paths.get("input"))).trim();
 		String[] rows = input.split("\\n");
 		HashMap<String, String> parents = new HashMap<>();
-		ArrayList<String> leafs = new ArrayList<>();
+		HashMap<String, String[]> children = new HashMap<>();
+		HashMap<String, Integer> weights = new HashMap<>();
 		for (int i = 0; i < rows.length; i++) {
 			String[] sides = rows[i].trim().split("->");
-			String parent[] = sides[0].trim().split("\\(");
+			String parts[] = sides[0].trim().split("\\(");
+			String parent = parts[0].trim();
+			int weight = Integer.parseInt(parts[1].split("\\)")[0]);
+			weights.put(parent, weight);
 			if (sides.length > 1) {
-				String[] children = sides[1].trim().split(",\\s*");
-				for (int j = 0; j < children.length; j++) {
-					parents.put(children[j], parent[0].trim());
+				String[] nodes = sides[1].trim().split(",\\s*");
+				for (int j = 0; j < nodes.length; j++) {
+					parents.put(nodes[j], parts[0].trim());
 				}
+				children.put(parent, nodes);
 			} else {
-				leafs.add(parent[0].trim());
+				children.put(parent, new String[] {});
 			}
 		}
-		String current = leafs.get(0);
+		String current = parents.keySet().iterator().next();
 		while (parents.containsKey(current)) {
 			current = parents.get(current);
 		}
--- day7/src/Question2.java
+++ day7/src/Question2.java
@@ -1,63 +1,82 @@
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.HashMap;
 import java.util.HashSet;
 
 public class Question2 {
 
+	public static int findCorrectedValue(HashMap<String, Integer> weights, HashMap<String, Integer> childWeights) {
+		HashMap<Integer, Integer> occurences = new HashMap<>();
+		for (String key : childWeights.keySet()) {
+			int childWeight = childWeights.get(key);
+			if (!occurences.containsKey(childWeight)) {
+				occurences.put(childWeight, 1);
+			} else {
+				occurences.put(childWeight, occurences.get(childWeight) + 1);
+			}
+		}
+		int faultyWeight = -1;
+		for (int weight : occurences.keySet()) {
+			if (occurences.get(weight) == 1) {
+				faultyWeight = weight;
+			}
+		}
+		String correct = null;
+		String faulty = null;
+		for (String key : childWeights.keySet()) {
+			if (childWeights.get(key) == faultyWeight) {
+				faulty = key;
+			} else {
+				correct = key;
+			}
+		}
+		int diff = childWeights.get(faulty) - childWeights.get(correct);
+		return weights.get(faulty) - diff;
+	}
+
+	public static int find(HashMap<String, Integer> weights, HashMap<String, String[]> children, String current) {
+		HashMap<String, Integer> childWeights = new HashMap<>();
+		int sum = 0;
+		for (String child : children.get(current)) {
+			int weight = find(weights, children, child);
+			childWeights.put(child, weight);
+			sum += weight;
+		}
+		if (new HashSet<Integer>(childWeights.values()).size() > 1) {
+			System.out.println(findCorrectedValue(weights, childWeights));
+			System.exit(0);
+		}
+		return sum + weights.get(current);
+	}
+
 	public static void main(String[] args) throws IOException {
 		String input = new String(Files.readAllBytes(Paths.get("input"))).trim();
 		String[] rows = input.split("\\n");
 		HashMap<String, String> parents = new HashMap<>();
+		HashMap<String, String[]> children = new HashMap<>();
 		HashMap<String, Integer> weights = new HashMap<>();
-		ArrayList<String> leafs = new ArrayList<>();
 		for (int i = 0; i < rows.length; i++) {
 			String[] sides = rows[i].trim().split("->");
-			String parent[] = sides[0].trim().split("\\(");
+			String parts[] = sides[0].trim().split("\\(");
+			String parent = parts[0].trim();
+			int weight = Integer.parseInt(parts[1].split("\\)")[0]);
+			weights.put(parent, weight);
 			if (sides.length > 1) {
-				String[] children = sides[1].trim().split(",\\s*");
-				for (int j = 0; j < children.length; j++) {
-					parents.put(children[j], parent[0].trim());
+				String[] nodes = sides[1].trim().split(",\\s*");
+				for (int j = 0; j < nodes.length; j++) {
+					parents.put(nodes[j], parts[0].trim());
 				}
+				children.put(parent, nodes);
 			} else {
-				leafs.add(parent[0].trim());
+				children.put(parent, new String[] {});
 			}
-			weights.put(parent[0].trim(), Integer.parseInt(parent[1].split("\\)")[0]));
 		}
-		while (true) {
-			HashMap<String, HashMap<String, Integer>> childWeights = new HashMap<>();
-			for (int i = 0; i < leafs.size(); i++) {
-				String child = leafs.get(i);
-				if (!parents.containsKey(child)) {
-					System.out.println(child);
-					System.exit(0);
-				}
-				String parent = parents.get(child);
-				if (!childWeights.containsKey(parent)) {
-					childWeights.put(parent, new HashMap<>());
-				}
-				HashMap<String, Integer> children = childWeights.get(parent);
-				children.put(child, weights.get(child));
-			}
-			for (String parent : childWeights.keySet()) {
-				HashMap<String, Integer> children = childWeights.get(parent);
-				int sum = 0;
-				for (int i : children.values()) {
-					sum += i;
-				}
-				weights.put(parent, weights.get(parent) + sum);
-				if (new HashSet<Integer>(children.values()).size() > 1) {
-					System.out.println(Arrays.toString(children.values().toArray(new Integer[] {})));
-					System.out.println(Arrays.toString(children.keySet().toArray(new String[] {})));
-					System.exit(0);
-				}
-			}
-			leafs = new ArrayList<String>(childWeights.keySet());
-			System.out.println(Arrays.toString(leafs.toArray(new String[] {})));
+		String current = parents.keySet().iterator().next();
+		while (parents.containsKey(current)) {
+			current = parents.get(current);
 		}
+		find(weights, children, current);
 	}
 
 }
