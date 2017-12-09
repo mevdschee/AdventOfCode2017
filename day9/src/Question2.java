75fc327b350a9848b1acd62ebbf28ca23db8190e
Maurits van der Schee <maurits@vdschee.nl>
2017-12-09 06:40:09 +0100

    Day 9

---
+++ day9/src/Question2.java
@@ -0,0 +1,46 @@
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+public class Question2 {
+
+	public static void main(String[] args) throws IOException {
+		String input = new String(Files.readAllBytes(Paths.get("input"))).trim();
+		String[] lines = input.split("\\n");
+		for (String line : lines) {
+			int depth = 0;
+			int count = 0;
+			int i = 0;
+			boolean garbage = false;
+			while (i < line.length()) {
+				if (!garbage) {
+					switch (line.charAt(i)) {
+					case '{':
+						depth++;
+						break;
+					case '<':
+						garbage = true;
+						break;
+					case '}':
+						depth--;
+						break;
+					}
+				} else {
+					switch (line.charAt(i)) {
+					case '!':
+						i++;
+						break;
+					case '>':
+						garbage = false;
+						break;
+					default:
+						count++;
+					}
+				}
+				i++;
+			}
+			System.out.println(count);
+		}
+	}
+
+}
