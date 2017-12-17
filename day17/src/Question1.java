4ff3a733867692eb7d8863b21a5d273037a664c9
Maurits van der Schee <maurits@vdschee.nl>
2017-12-17 06:28:32 +0100

    Day 17

---
+++ day17/src/Question1.java
@@ -0,0 +1,20 @@
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+
+public class Question1 {
+
+	public static void main(String[] args) throws IOException {
+		int step = Integer.parseInt(new String(Files.readAllBytes(Paths.get("input"))).trim());
+		ArrayList<Integer> buffer = new ArrayList<>();
+		buffer.add(0);
+		int p = 0;
+		for (int i = 1; i <= 2017; i++) {
+			p = (p + step) % buffer.size() + 1;
+			buffer.add(p, i);
+		}
+		System.out.println(buffer.get((p + 1)% buffer.size()));
+	}
+
+}
---
+++ day17/src/Question2.java
@@ -0,0 +1,20 @@
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+public class Question2 {
+
+	public static void main(String[] args) throws IOException {
+		int step = Integer.parseInt(new String(Files.readAllBytes(Paths.get("input"))).trim());
+		int buffer = 0;
+		int p = 0;
+		for (int i = 1; i <= 50000000; i++) {
+			p = (p + step) % i + 1;
+			if (p == 1) {
+				buffer = i;
+			}
+		}
+		System.out.println(buffer);
+	}
+
+}
